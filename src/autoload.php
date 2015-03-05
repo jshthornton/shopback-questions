45b926b264a64b51a1e2967ff9dfebdbad43812b
Joshua Thornton <joshua.thornton@readingroom.com>
2015-03-05 15:28:17 +0800

    Setting up phpunit core config.

---
+++ src/autoload.php
@@ -0,0 +1,9 @@
+<?php
+spl_autoload_register(function($class) {
+	if(file_exists(__DIR__ . DIRECTORY_SEPARATOR . $class . '.php')) {
+		include __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
+	} else {
+		@include $class . '.php';
+	}
+});
+?>

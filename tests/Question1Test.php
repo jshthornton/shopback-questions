cdef930cd5553a10fc5510c82d66d7fa977df5fa
Joshua Thornton <joshua.thornton@readingroom.com>
2015-03-05 15:31:25 +0800

    Test to ensure that the data is in ascending order in the array.

--- tests/Question1Test.php
+++ tests/Question1Test.php
@@ -6,4 +6,16 @@ class Question1Test extends PHPUnit_Framework_TestCase {
 		$question->populate(5);
 		$this->assertEquals(count($question->arr), 5);
 	}
+
+	public function testShouldPopulateAscending() {
+		$question = new Question1();
+		$question->populate(5);
+
+		$arr = $question->arr;
+		$this->assertEquals($arr[0], 1);
+		$this->assertEquals($arr[1], 2);
+		$this->assertEquals($arr[2], 3);
+		$this->assertEquals($arr[3], 4);
+		$this->assertEquals($arr[4], 5);
+	}
 }
